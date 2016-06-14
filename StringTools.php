b8771acc87dd050c94e3257aff8fcbff6c8bc576
Nek (Maxime Veber) <nek.dev@gmail.com>
2016-06-14 12:53:58 +0200

    Add new useful methods to string tools

--- StringTools.php
+++ StringTools.php
@@ -4,12 +4,56 @@ namespace Nekland\Tools;
 
 class StringTools
 {
-    public static function camelize($str, $from='_')
+    /**
+     * @param string $str
+     * @param string $from
+     * @return string
+     */
+    public static function camelize($str, $from = '_')
     {
         if (!in_array($from, ['_', '-'])) {
             throw new \InvalidArgumentException('We can camelize only from snake case or kebab case.');
         }
 
         return implode('', array_map('ucfirst', array_map('strtolower', explode($from, $str))));
+    }
+
+    /**
+     * @param string $str
+     * @param string $start
+     * @return bool
+     */
+    public static function startsWith($str, $start)
+    {
+        $length = mb_strlen($start);
+        return substr($str, 0, $length) === $start;
+    }
+
+    /**
+     * @param string $str
+     * @param string $end
+     * @return bool
+     */
+    public static function endsWith($str, $end)
+    {
+        $length = mb_strlen($end);
+        if ($length === 0) {
+            return true;
+        }
+        return substr($str, -$length) === $end;
+    }
+
+    /**
+     * @param string $str
+     * @param string $toRemove
+     * @return string
+     */
+    public static function removeStart($str, $toRemove)
+    {
+        if (!StringTools::startsWith($str, $toRemove)) {
+            return $str;
+        }
+
+        return mb_substr($str, mb_strlen($toRemove));
     }
 }
--- spec/Nekland/Tools/StringToolsSpec.php
+++ spec/Nekland/Tools/StringToolsSpec.php
@@ -26,4 +26,28 @@ class StringToolsSpec extends ObjectBehavior
     {
         $this::shouldThrow('\InvalidArgumentException')->duringCamelize('hello@world', '@');
     }
+
+    function it_should_check_if_string_starts_with_needle()
+    {
+        $this::startsWith('Lorem ipsum heyà', 'Lorem')->shouldReturn(true);
+        $this::startsWith(' Lorem ipsum heyà', 'Lorem')->shouldReturn(false);
+        $this::startsWith("\nLorem ipsum heyà", 'Lorem')->shouldReturn(false);
+        $this::startsWith(' ipsum heyà Lorem', 'Lorem')->shouldReturn(false);
+        $this::startsWith(' ipsum heyà Lorem', '')->shouldReturn(true);
+    }
+
+    function it_should_check_if_string_ends_with_needle()
+    {
+        $this::endsWith('Foo bar baz ça marche', 'marche')->shouldReturn(true);
+        $this::endsWith('Foo bar baz ça marche ', 'marche')->shouldReturn(false);
+        $this::endsWith('Foo bar marche baz ça ', 'marche')->shouldReturn(false);
+        $this::endsWith('marche Foo bar baz ça ', 'marche')->shouldReturn(false);
+        $this::endsWith('marche Foo bar baz ça ', '')->shouldReturn(true);
+    }
+
+    function it_should_remove_the_start_of_a_string()
+    {
+        $this::removeStart('Foo bar baz', 'Foo')->shouldReturn(' bar baz');
+        $this::removeStart('YOLOOOsgs gs gsg sggs g', 'g')->shouldReturn('YOLOOOsgs gs gsg sggs g');
+    }
 }
